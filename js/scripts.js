1dfe017c795842adde2dc6992cbc0ac48ad12c59
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2022-06-13 15:04:37 -0400

    Added pokemonList for Exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'stir fry';
-document.write(favoriteFood);
+let pokemonList = [
+    {name : 'Bulbasaur', height : '7', types : ['grass', 'poison']},
+    {name : 'Charmander', height : '6', types : 'fire'},
+    {name : 'Squirtle', height : '5', types : 'water'}
+]
+
+document.write(pokemonList);
