f6506b13abaff40838fa936767fc1a72739a811a
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2022-07-22 18:27:08 -0400

    Added conditional loop for 1.3 JS Basics task

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 let pokemonList = [
-    {name : 'Bulbasaur', height : '7', types : ['grass', 'poison']},
-    {name : 'Charmander', height : '6', types : 'fire'},
-    {name : 'Squirtle', height : '5', types : 'water'}
+    {name: 'Bulbasaur', height: '7', types: ['grass', 'poison']},
+    {name: 'Charmander', height: '6', types: 'fire'},
+    {name: 'Squirtle', height: '5', types: 'water'}
 ]
-
-document.write(pokemonList);
+// loop with conditional that adds special comment for any Pokemon larger than "7" in height
+for (let i = 0; i <= pokemonList.length; i++) {
+    if (pokemonList[i].height < 7){
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ')<br>')
+    }else if (pokemonList[i].height >= 7){
+        document.write(pokemonList[i].name + ' (height:' + pokemonList[i].height + ') - Wow, that is big! <br>')
+    }
+}
